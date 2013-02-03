0c3e4c27e95f6e5c3f1951fda6098198f365b997
achae <achae@ubuntu.ubuntu-domain>
2013-02-03 00:48:42 -0500

    Fixing post-Jia email

--- public/javascripts/orders.js
+++ public/javascripts/orders.js
@@ -1,12 +1,13 @@
 $(function () {
+
   $('#neworder').on('submit', function () {
     $.post("/order/create", $('#neworder').serialize());
     return false;
   })
 
-  $('#delorder').on('submit', function () {
-    $.post("/order/delete", $(#delorder).serialize());
+  $('#delorder').on('click', function () {
+    $.post("/order/delete", $('#delorder').serialize());
     $(this).remove();
     return false;
   })
-})
+}
